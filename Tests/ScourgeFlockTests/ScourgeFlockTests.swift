0eafd4f440c570d848dd8485aa3ae59b045a3c8a
Bastian Inuk Christensen-Knuth <jonalist@gmail.com>
2025-10-19 18:24:00 +0200

    make sure the name doesn't override the name

--- Tests/ScourgeFlockTests/ScourgeFlockTests.swift
+++ Tests/ScourgeFlockTests/ScourgeFlockTests.swift
@@ -8,10 +8,18 @@ struct TestService_Struct {}
 @Flock
 class TestService_Class {}
 
+@Flock
+struct TestService_ExistingName {
+    public let name: String = "CustomName"
+}
+
 @Test func macroExpands() async throws {
     let serviceStruct = Service(TestService_Struct())
     #expect(serviceStruct.definition.name == "TestService_Struct")
 
     let serviceClass = Service(TestService_Class())
     #expect(serviceClass.definition.name == "TestService_Class")
+
+    let serviceExisting = Service(TestService_ExistingName())
+    #expect(serviceExisting.definition.name == "CustomName")
 }
